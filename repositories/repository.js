5b61f4b36365e59e677d4b255854e9c7e70d8ecd
Diego Santos <diegodev.silva@gmail.com>
2023-11-06 09:42:15 -0300

    aplicação servidor

---
+++ repositories/repository.js
@@ -0,0 +1,49 @@
+// importtar o fs
+// criar os metodos para salvar o nosso user
+
+const fs = require("fs");
+
+// classe com um construtor que cria o nosso arquivo
+// usamos o modele.exports
+
+
+// este repositorio sera utilizado para criar os metodos comuns
+
+module.exports = class Repository {
+  // construtor, verificar se recebo um nom
+
+  constructor(filename) {
+    if (!filename) {
+      throw new Error("voce precisa informar o nome de um  arquivo!");
+    }
+    //atributo da classe
+    this.filename = filename;
+    try {
+      fs.accessSync(this.filename);
+    } catch (error) {
+      fs.writeFileSync(this.filename, "[]");
+    }
+  }
+  // nossos metodos
+async create(atributos){
+    //ler todos os dados de um arquivo
+const records = await this.getAll();
+//os dados e uma lista, add os dados em uma lista
+    records.push(atributos)
+
+await this.writeAll(records)
+
+
+}
+
+
+  async getAll(){
+    return JSON.parse(await fs.promises.readFile(this.filename))
+  }
+
+  async writeAll(records){
+        await fs.promises.writeFile(this.filename, JSON.stringify(records))
+  }
+
+
+};
---
+++ routes/auth.js
@@ -0,0 +1,57 @@
+const express = require ("express")
+
+const router = express.Router()
+
+const userRepo = require('../repositories/user')
+const prodRepo = require('../repositories/products')
+
+const screenSignup = require('../VIEWS/signup')
+const screenLogin = require('../VIEWS/login')
+const screenLayout = require('../VIEWS/prod-layout')
+
+router.post('/add-login', (req, res) => {
+    //devolver uma tela com produtos feitos
+    // verificar user
+    res.send("finalizando")
+})
+
+router.get('/login', (req, res) => {
+    res.send(screenLogin())
+})
+
+
+
+router.post('/add-signup', (req, res) => {
+
+  
+    userRepo.create(req.body)
+
+
+    /* PEGAR OS DADOS QUE VEM DE JAON (EMAIL, NOME E SENHA) 
+    E SALVAR EM UM ARQUIVO JSON*/
+
+    /*1 - Importar fs
+   2 - fazer o metodo create - criar o user */
+
+
+
+
+    res.send("cadastro criado")
+})
+
+
+router.get('/signup', (req, res) => {
+    res.send(screenSignup())
+})
+
+router.post('/prod-layout', (req, res) => {
+
+  
+    prodRepo.create(req.body)
+
+    res.send("cadastro criado")
+})
+
+
+
+module.exports = router
